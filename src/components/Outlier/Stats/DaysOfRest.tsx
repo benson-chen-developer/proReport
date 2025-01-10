2b13d1dfcf8b6ac2ac3fede71133e834032971e1
Benson Chen <benson.chen.1@stonybrook.edu>
2025-01-10 00:04:01 -0500

    made the daysrested have an exited btn

--- src/components/Outlier/Stats/DaysOfRest.tsx
+++ src/components/Outlier/Stats/DaysOfRest.tsx
@@ -49,13 +49,39 @@ export const DaysOfRest: React.FC<Props> = ({filter, setFilter, ourPlayer}) => {
                 cursor:'pointer', userSelect: 'none',
               }}
               ref={buttonRef}
-              onClick={() => setIsPopUp(p => !p)}
           >
-              <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24"><path fill="#fff" d="M13 3a9 9 0 0 0-9 9H1l3.89 3.89l.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7s-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42A8.95 8.95 0 0 0 13 21a9 9 0 0 0 0-18m-1 5v5l4.28 2.54l.72-1.21l-3.5-2.08V8z"/></svg>
-              <p style={{color:'#B1B1B1', fontSize:'12px', fontWeight:'bold'}}>{
-                selectedText ? selectedText :'Days Rested'
-              }</p>
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="" fillRule="evenodd"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="#fff" d="M13.06 16.06a1.5 1.5 0 0 1-2.12 0l-5.658-5.656a1.5 1.5 0 1 1 2.122-2.121L12 12.879l4.596-4.596a1.5 1.5 0 0 1 2.122 2.12l-5.657 5.658Z"/></g></svg>
+              <div 
+                onClick={() => setIsPopUp(p => !p)}
+                style={{height:'100%', display:'flex', alignItems:'center', marginLeft:'5px'}}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24"><path fill="#fff" d="M13 3a9 9 0 0 0-9 9H1l3.89 3.89l.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7s-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42A8.95 8.95 0 0 0 13 21a9 9 0 0 0 0-18m-1 5v5l4.28 2.54l.72-1.21l-3.5-2.08V8z"/></svg>
+              </div>
+             
+              <div 
+                onClick={() => setIsPopUp(p => !p)}
+                style={{height:'100%', width:'100%', display:'flex', alignItems:'center', justifyContent:'center'}}
+              >
+                <p style={{color:'#B1B1B1', fontSize:'12px', fontWeight:'bold'}}>
+                  {selectedText ? selectedText :'Days Rested'}
+                </p>
+              </div>
+
+              {selectedText !== 'Days Rested' ? 
+                  <div onClick={() => {
+                    setSelectedText('Days Rested')
+                    setFilter(p => ({...p, daysRested: -1}))
+                    setIsPopUp(false)
+                  }} style={{height:'100%', display:'flex', alignItems:'center', marginRight:'5px'}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#fff" d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10s10-4.486 10-10S17.514 2 12 2m4.207 12.793l-1.414 1.414L12 13.414l-2.793 2.793l-1.414-1.414L10.586 12L7.793 9.207l1.414-1.414L12 10.586l2.793-2.793l1.414 1.414L13.414 12z"/></svg>
+                  </div>
+                  :
+                <div 
+                  onClick={() => setIsPopUp(p => !p)}
+                  style={{height:'100%', display:'flex', alignItems:'center', marginRight:'5px'}}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="" fillRule="evenodd"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="#fff" d="M13.06 16.06a1.5 1.5 0 0 1-2.12 0l-5.658-5.656a1.5 1.5 0 1 1 2.122-2.121L12 12.879l4.596-4.596a1.5 1.5 0 0 1 2.122 2.12l-5.657 5.658Z"/></g></svg>
+                </div>
+              }
           </div>
 
           {/* Pop Up */}
